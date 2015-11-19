f927e7f8521cc6a14ff5da1fd66e889ab1c54d59
Rebecca Vreeland <rebeccavreeland@gmail.com>
2015-11-18 19:08:49 -0800

    Add bootstrap (via CDN) Add bootswatch Sandstone (via CDN) Re-style gearList View

--- client/app/components/gearlist/gearlistController.js
+++ client/app/components/gearlist/gearlistController.js
@@ -10,7 +10,9 @@
     function Gearlist() {
       var vm = this;
       //mock data
-      vm.gear = [{name: 'Marmot Limelight 1', type: 'tent', category: 'shelter', weight: '5 lbs'}];
+      vm.gear = [{name: 'Marmot Limelight 2', type: 'tent', category: 'shelter', weight: '80'},
+      {name: 'Olicamp Light', type: 'stove', category: 'kitchen', weight: '2.5'},
+      {name: 'Patagonia down jacket', type: 'jacket', category: 'clothing', weight: '12'}];
 
     }
 
