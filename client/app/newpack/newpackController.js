631dc4fecf685a17a0ad30f4839b0872503dcff7
Rebecca Vreeland <rebeccavreeland@gmail.com>
2015-12-17 14:11:27 -0800

    Double click to add item to gear list. Update base weight.

--- client/app/newpack/newpackController.js
+++ client/app/newpack/newpackController.js
@@ -11,12 +11,21 @@
     var vm = this;
 
     vm.gear = dataService.getGearlist();
+    //set initially to false
+    vm.packCreated = true;
 
-    vm.packCreated = false;
-
-    //properties: name, season, weight
+    //properties: name, season, weight, items
+    //mock
     vm.pack = {
-      weight: 0
+      name: 'Hoover Wilderness',
+      season: 'summer',
+      weight: 0,
+      items: []
+    };
+
+    vm.packWeight = {
+      lbs: 0,
+      oz:0
     };
 
     vm.createPack = function() {
@@ -24,6 +33,14 @@
       vm.packCreated = true;
     };
 
+    vm.addToPack = function(item) {
+      vm.pack.items.push(item);
+      vm.pack.weight += parseFloat(item.weight);
+      vm.packWeight = {
+        lbs: parseInt(vm.pack.weight/16),
+        oz: parseFloat(vm.pack.weight%16)
+      };
+    };
 
   }
 
